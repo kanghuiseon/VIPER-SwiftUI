80c8318ced5f801a4b844dcbb8fb6a5bfb4b2d20
heeseon <heeseon@peoplefund.co.kr>
2022-09-01 23:24:12 +0900

    project init

---
+++ VIPER-Swift/VIPER-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VIPER-Swift
+//
+//  Created by 강희선 on 2022/09/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VIPER-Swift/VIPER-Swift/VIPER_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIPER_SwiftApp.swift
+//  VIPER-Swift
+//
+//  Created by 강희선 on 2022/09/01.
+//
+
+import SwiftUI
+
+@main
+struct VIPER_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
