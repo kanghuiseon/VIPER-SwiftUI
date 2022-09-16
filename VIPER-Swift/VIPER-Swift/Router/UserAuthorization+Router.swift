2c100051eb78d40731d891046a1c9922de9db81b
heeseon <sweetheeseon@gmail.com>
2022-09-17 00:08:18 +0900

    add Authorization router, view

---
+++ VIPER-Swift/VIPER-Swift/Router/UserAuthorization+Router.swift
@@ -0,0 +1,23 @@
+//
+//  Authorization+Router.swift
+//  VIPER-Swift
+//
+//  Created by 강희선 on 2022/09/14.
+//
+
+import Foundation
+
+protocol UserAuthorizationRouter {
+    func start()
+    func exit()
+}
+
+class RealUserAuthorizationRouter: UserAuthorizationRouter {
+    func start() {
+        
+    }
+    
+    func exit() {
+        
+    }
+}
---
+++ VIPER-Swift/VIPER-Swift/View/Authentication/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  VIPER-Swift
+//
+//  Created by 강희선 on 2022/09/03.
+//
+
+import SwiftUI
+import DIContainer
+
+struct LoginView: View {
+    var body: some View {
+        Text("need Login!")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
